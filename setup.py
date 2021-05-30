7fa2c02666b351e51be8d080ecdb038eff188acf
Shubham Joshi <shubhamjoshi2130@gmail.com>
2021-05-30 18:35:59 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ineuron demo',
+    author='shubhamjoshi2130',
+    license='MIT',
+)
